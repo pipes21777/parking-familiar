01ef854756c8425c7874f432b2cce53660d76e47
pipes21777 <pipes21777@users.noreply.github.com>
2026-05-30 23:24:18 +0200

    SW amb fetch handler

--- sw.js
+++ sw.js
@@ -1,2 +1,6 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
